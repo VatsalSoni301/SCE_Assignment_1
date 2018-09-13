9e685fe1a4ed0b96a42951430228a5f029160cf8
Vatsal Soni <vatsal.soni@students.iiit.ac.in>
2018-09-13 19:26:50 +0530

    done1

--- 4_3.sh
+++ 4_3.sh
@@ -4,7 +4,7 @@ read -p "Enter Username: " uname
 echo -e "Enter Password: \c"
 read -s passwd
 echo $uname > vpnpass.txt
-echo $passwd > vpnpass.txt
+echo $passwd >> vpnpass.txt
 #echo $root >> vpnpass.txt
 wget --http-user=$uname --http-password=$passwd https://vpn.iiit.ac.in/secure/ubuntu.ovpn
 sudo openvpn --config ubuntu.ovpn --auth-user-pass vpnpass.txt
